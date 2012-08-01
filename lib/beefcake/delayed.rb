6bb42a97d5224a2efd2765fa23c0b6a2f2f9fd16
TJ Murphy <teej.murphy@gmail.com>
2012-07-31 18:30:27 -0700

    Adding object masquerading to delayed

--- lib/beefcake/delayed.rb
+++ lib/beefcake/delayed.rb
@@ -1,19 +1,44 @@
 module Beefcake
 
   class Delayed
-    attr_accessor :klass, :bytes
+    attr_accessor :klass, :bytes, :object
     
     def initialize(klss, byts)
-      self.klass, self.bytes = klss, byts
+      @klass, @bytes = klss, byts
     end
     
     def decode
-      Object.const_get(self.klass).decode(self.bytes)
+      __object__
     end
     
     def encode
-      self.bytes
+      (@object) ? @object.encode : @bytes
     end
+    
+    def __object__
+      @object = Object.const_get(@klass).decode(@bytes.dup) unless @object
+      @object
+    end
+    
+    alias :_inspect :inspect
+    def inspect
+      if @object
+        @object.inspect
+      else
+        _inspect
+      end
+    end
+    
+    def respond_to?(message)
+      message = message.to_sym
+      [:__object__, :inspect].include?(message) ||
+        __object__.respond_to?(message)
+    end
+    
+    def method_missing(*a, &b)
+      __object__.__send__(*a, &b)
+    end
+    
   end
   
 end
